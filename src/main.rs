c7edc222a55efe4f9240433b3b4909a6a4f559d3
millouwmills <1HLvsUiXtsMOX>
2021-04-02 17:27:28 +0200

    Did Array Destruction (1474C)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[allow(non_snake_case)]
 #[allow(dead_code)]
-mod going_home_1500A;
+mod array_destruction_1474C;
 
 fn main() {
-    going_home_1500A::main();
+    array_destruction_1474C::main();
 }
